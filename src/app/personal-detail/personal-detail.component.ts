c9c1e5cb031359b404acf4efdb579c9a17cf1056
Calla Rudolph and Michelle Poterek <calla@>
2017-08-23 10:15:44 -0700

    display personalId in dynamic route

--- src/app/personal-detail/personal-detail.component.ts
+++ src/app/personal-detail/personal-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 import { Personal } from '../personal.model';
 import { Router } from '@angular/router';
 
@@ -8,10 +10,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./personal-detail.component.css']
 })
 export class PersonalDetailComponent implements OnInit {
+  personalId: number = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+  this.personalId = parseInt(urlParameters['id']);
+});
   }
 
 }
